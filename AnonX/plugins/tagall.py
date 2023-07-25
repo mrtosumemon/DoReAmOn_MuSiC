684f59423251573a7c2a8d3255094a3618ecec90
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-07-25 05:32:01 +0530

    Update tagall.py

--- AnonX/plugins/tagall.py
+++ AnonX/plugins/tagall.py
@@ -191,7 +191,7 @@ async def mentionall(client, message):
     except:
         pass
 
-@app.on_message(filters.command(["cancel", "stop"]))
+@app.on_message(filters.command(["ecancel", "cancel"]))
 async def cancel_spam(client, message):
     if not message.chat.id in spam_chats:
         return await message.reply("No active mention process is started by me.")
